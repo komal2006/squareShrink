650bcb5d803d91acbdb0befd9fac117199022353
Komal2006 <komaldeepkr99@gmail.com>
2020-01-13 20:35:25 -0500

    add slider

--- squareShrink/ViewController.swift
+++ squareShrink/ViewController.swift
@@ -12,16 +12,44 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var lblCount: UILabel!
     @IBOutlet weak var square: UIView!
+     var speed = 0.1
+    @IBOutlet var sliderLbl: UIView!
+   
+    @IBAction func slider(_ sender: UISlider) {
+    
+    switch sender.value
+        {
+             case 0:
+                  speed = 1.2
+                  print(speed)
+                  break
+                case 1:
+                  speed = 1.0
+                  print(speed)
+                  break
+                case 2:
+                  speed = 0.8
+                  break
+                case 3:
+                  speed = 0.6
+                    break
+                case 4:
+                  speed = 0.4
+                   
+                default:
+                  speed = 0.1
+             }
+    }
     var timer: Timer!
     var c = 0
     override func viewDidLoad() {
         super.viewDidLoad()
         lblCount.text = "\(c)"
         // Do any additional setup after loading the view.
-         let mainViewHeight = view.frame.size.height
-             let mainViewWidth = view.frame.size.width
-             var myViewHeight = self.square.frame.size.height
-             var myViewWidth = self.square.frame.size.width
+//         let mainViewHeight = view.frame.size.height
+//             let mainViewWidth = view.frame.size.width
+//             var myViewHeight = self.square.frame.size.height
+//             var myViewWidth = self.square.frame.size.width
              square.center = view.center
              square.backgroundColor = .red
              var timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true)
@@ -33,7 +61,7 @@ class ViewController: UIViewController {
           
             
            func expand(){
-             var timer = Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true)
+             var timer = Timer.scheduledTimer(withTimeInterval: speed, repeats: true)
              { timer in
                   // self.myView.backgroundColor = .random()
                         print("Expand by\(timer)")
@@ -54,7 +82,7 @@ class ViewController: UIViewController {
             
             self.c+=1
             lblCount.text = "\(c)"
-            var timer = Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true)
+            var timer = Timer.scheduledTimer(withTimeInterval: speed, repeats: true)
             { timer in
               //self.myView.backgroundColor = .random()
                     print("Expand by\(timer)")
